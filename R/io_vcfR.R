6a1d1f1ab468befedd9679fc6bd029e4119ff2a0
knausb <briank.lists@gmail.com>
2015-02-17 14:58:42 -0600

    updated read write vcf

--- R/io_vcfR.R
+++ R/io_vcfR.R
@@ -5,9 +5,13 @@
 #' @export
 #'
 #' @description
-#' Reads and writes files in the vcf format.
+#' Read and write files in the vcf format.
 #' 
-#' @param x A filename for a variant call format (vcf) file
+#' @param file A filename for a variant call format (vcf) file
+#' @param xvcf A vcfR object
+# @param vfile an output filename
+#' @param mask logical vector indicating rows to use
+#' @param APPEND logical indicating whether to append to existing vcf file or write a new file
 #'
 #' @details
 #' Reads in a vcf file and stores it in a vcf class.  Once the number of lines the meta information contains the data is divided into three tables: meta data, fixed data and genotype data.
@@ -21,20 +25,20 @@
 #' plot(pinf_vcf)
 #' pinf_vcf[1:6,]
 #' 
-read.vcf<-function(x){
+read.vcf<-function(file){
   vcf <- new(Class="vcfR")
   i <- -1 # Line counter.
   j <- 0 # Success?
-  tmp <- scan(x, what="character", sep="\n", skip=0, nlines=1, quiet=T, comment.char="")
+  tmp <- scan(file, what="character", sep="\n", skip=0, nlines=1, quiet=T, comment.char="")
   if(length(grep('^##', tmp)) >0 ){
     while(j == 0){
       i <- i+1
-      tmp <- scan(x, what="character", sep="\n", skip=i, nlines=1, quiet=T, comment.char="")
+      tmp <- scan(file, what="character", sep="\n", skip=i, nlines=1, quiet=T, comment.char="")
       if(length(grep('^##',tmp)) == 0){j <- 1}
     }
-    vcf@meta <- scan(x, what = "character", sep = "\n", skip = 0, nlines = i,
+    vcf@meta <- scan(file, what = "character", sep = "\n", skip = 0, nlines = i,
                      quiet = T, comment.char = "")
-    vcf@fix <- read.table(x, header = TRUE, sep = '\t', skip = i, comment.char = '',
+    vcf@fix <- read.table(file, header = TRUE, sep = '\t', skip = i, comment.char = '',
                           colClasses = "character", check.names = FALSE)
     vcf@gt <- vcf@fix[,9:ncol(vcf@fix)]
     vcf@fix <- vcf@fix[,1:8]
@@ -42,7 +46,7 @@ read.vcf<-function(x){
     vcf@fix$QUAL <- as.integer(vcf@fix$QUAL)
   } else if (length(grep('^#',tmp)) >0 ){
     # No meta region, but a header line.
-    vcf@fix <- read.table(x,header=T,sep='\t',skip=0,comment.char='', colClasses = "character")
+    vcf@fix <- read.table(file, header=T, sep='\t', skip=0,comment.char='', colClasses = "character")
     vcf@gt <- vcf@fix[,9:ncol(vcf@fix)]
     vcf@fix <- vcf@fix[,1:8]
     #    colnames(vcf@fix) <- c('chrom','pos','id','ref','alt','qual','filter','info')
@@ -51,12 +55,15 @@ read.vcf<-function(x){
     vcf@fix$QUAL <- as.integer(vcf@fix$QUAL)
   } else {
     # No meta region or header line.
-    vcf@fix <- read.table(x,header=F,sep='\t',skip=0,comment.char='', colClasses = "character")
+    vcf@fix <- read.table(file,header=F,sep='\t',skip=0,comment.char='', colClasses = "character")
     vcf@gt <- vcf@fix[,9:ncol(vcf@fix)]
     vcf@fix <- vcf@fix[,1:8]
     colnames(vcf@fix) <- c('CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO')
     vcf@fix$POS  <- as.integer(vcf@fix$POS)
     vcf@fix$QUAL <- as.integer(vcf@fix$QUAL)    
+  }
+  if(names(vcf@fix)[1] != "CHROM"){
+    names(vcf@fix)[1] <- "CHROM"
   }
   return(vcf)
 }
@@ -69,15 +76,11 @@ read.vcf<-function(x){
 #' 
 # @usage write.vcf(xvcf, vfile)
 #' 
-#' @param xvcf A vcfR object
-# @param vfile an output filename
-#' @param mask logical vector indicating rows to use
-#' @param APPEND logical indicating whether to append to existing vcf file or write a new file
 #' 
 #' @export
 #' 
 #write.vcf<-function(xvcf, vfile, mask=logical(0), APPEND=FALSE){
-write.vcf<-function(xvcf, x, mask=logical(0), APPEND=FALSE){
+write.vcf<-function(xvcf, file=file, mask=logical(0), APPEND=FALSE){
   if(class(xvcf) == 'Chrom'){
     # Recast as a vcfR object.
     temp <- xvcf
@@ -102,21 +105,21 @@ write.vcf<-function(xvcf, x, mask=logical(0), APPEND=FALSE){
   if(APPEND == FALSE){
     header <- c(names(xvcf@fix), names(xvcf@gt))
     header[1] <- paste("#",header[1],sep='')
-    write.table(xvcf@meta, file = vfile, append = FALSE, quote = FALSE, sep = "\t",
+    write.table(xvcf@meta, file = file, append = FALSE, quote = FALSE, sep = "\t",
                 eol = "\n", na = "NA", dec = ".", row.names = FALSE,
                 col.names = FALSE)
-    write(header, file = vfile,
+    write(header, file = file,
           ncolumns=length(header),
           append = TRUE,
           sep = "\t")
-    write.table(cbind(xvcf@fix[mask,], xvcf@gt[mask,]), file = vfile, append = TRUE,
+    write.table(cbind(xvcf@fix[mask,], xvcf@gt[mask,]), file = file, append = TRUE,
                 quote = FALSE, sep = "\t",
                 eol = "\n", na = "NA", dec = ".",
                 row.names = FALSE,
                 col.names = FALSE)
     #              col.names = TRUE)
   } else if (APPEND == TRUE){
-    write.table(cbind(xvcf@fix[mask,], xvcf@gt[mask,]), file = vfile, append = TRUE,
+    write.table(cbind(xvcf@fix[mask,], xvcf@gt[mask,]), file = file, append = TRUE,
                 quote = FALSE, sep = "\t",
                 eol = "\n", na = "NA", dec = ".",
                 row.names = FALSE,
@@ -129,10 +132,10 @@ write.vcf<-function(xvcf, x, mask=logical(0), APPEND=FALSE){
 #' @rdname io_vcfR
 #' @export
 #' 
-read.vcf.devel <- function(x){
+read.vcf.devel <- function(file){
   vcf <- new(Class="vcfR")
-  vcf@meta <- .Call('vcfR_readVcfHeader', PACKAGE = 'vcfR', x)
-  temp <- .Call('vcfR_readVcfBody', PACKAGE = 'vcfR', x)
+  vcf@meta <- .Call('vcfR_readVcfHeader', PACKAGE = 'vcfR', file)
+  temp <- .Call('vcfR_readVcfBody', PACKAGE = 'vcfR', file)
   vcf@fix <- as.data.frame(temp[-1,1:8])
   names(vcf@fix) <- temp[1,1:8]
   vcf@gt <- as.data.frame(temp[-1,-c(1:8)])
--- tests/misc.R
+++ tests/misc.R
@@ -1,3 +1,22 @@
 
 library(vcfR)
 
+data(vcfR_example)
+
+head(pinf_vcf)
+
+
+working_dir <- getwd()
+
+setwd(tempdir())
+
+write.vcf(pinf_vcf, "pinf_mt.vcf")
+x <- read.vcf("pinf_mt.vcf")
+
+head(x)
+
+unlink("pinf_mt.vcf")
+
+setwd(working_dir)
+
+
