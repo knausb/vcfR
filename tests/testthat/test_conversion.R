a8293a7d6072b4213b1a1b7d74545b15f6023e77
knausb <briank.lists@gmail.com>
2015-03-12 16:56:48 -0700

    added test_conversion.R and test_extract_gt.R

---
+++ tests/testthat/test_conversion.R
@@ -0,0 +1,18 @@
+# create_chrom tests.
+
+# detach(package:vcfR, unload=T)
+library(vcfR)
+context("windowing functions")
+
+data(vcfR_example)
+
+pinf_mt <- create_chrom('pinf_mt', seq=pinf_dna, vcf=pinf_vcf, ann=pinf_gff, verbose=F)
+pinf_mt <- proc_chrom(pinf_mt, verbose=FALSE)
+
+gt <- extract.gt(pinf_mt, element="PL", as.numeric=TRUE)
+
+head(pinf_mt@vcf.gt)
+
+head(gt)
+
+
---
+++ tests/testthat/test_extract_gt.R
@@ -0,0 +1,57 @@
+# extractgt devel
+
+#detach(package:vcfR, unload=TRUE)
+library(vcfR)
+context("extract_gt functions")
+
+data(vcfR_example)
+
+
+pinf_mt <- create_chrom('pinf_mt', seq=pinf_dna, vcf=pinf_vcf, ann=pinf_gff, verbose=F)
+pinf_mt <- proc_chrom(pinf_mt, verbose=FALSE)
+
+pinf_mt <- create_chrom('pinf_mt', seq=pinf_dna, vcf=pinf_vcf, ann=pinf_gff, verbose=F)
+pinf_mt <- proc_chrom(pinf_mt, verbose=FALSE)
+
+
+gt <- extract.gt(pinf_vcf, element="GT", as.numeric=FALSE)
+pl <- extract.gt(pinf_vcf, element="PL", as.numeric=FALSE)
+gq <- extract.gt(pinf_vcf, element="GQ", as.numeric=TRUE)
+
+
+head(gt)
+head(pl)
+head(gq)
+
+head(pinf_mt@vcf.gt)
+
+
+#head(pinf_vcf)
+
+
+#ncol(pinf_vcf@gt)
+
+outm <- .Call('vcfR_extract_GT_to_DF', PACKAGE = 'vcfR', pinf_vcf@gt, element="GQ")
+outm <- .Call('vcfR_extract_GT_to_DF', PACKAGE = 'vcfR', pinf_vcf@gt, element="GT")
+outm <- .Call('vcfR_extract_GT_to_DF', PACKAGE = 'vcfR', pinf_vcf@gt, element="PL")
+outm <- .Call('vcfR_extract_GT_to_DF', PACKAGE = 'vcfR', pinf_vcf@gt, element="DP")
+
+
+outm <- .Call('vcfR_extract_GT_to_CM', PACKAGE = 'vcfR', pinf_vcf@gt, element="DP")
+
+#outm <- extract.gt2(pinf_vcf, element="DP", as.numeric=F)
+#outm <- extract.gt2(pinf_vcf, element="GQ", as.numeric=T)
+
+#head(pinf_vcf@gt)
+#head(outm)
+
+
+
+#outm <- .Call('vcfR_extract_GT_to_CM', PACKAGE = 'vcfR', pinf_vcf@gt, element="GQ")
+
+#head(outm)
+
+
+#outm2 <- .Call('vcfR_CM_to_NM', PACKAGE = 'vcfR', outm)
+#head(outm2)
+
