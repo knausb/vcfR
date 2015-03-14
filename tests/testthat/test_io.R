bb1fad2f1ee5d0bdc44180000c8899e47e9b6b4d
knausb <briank.lists@gmail.com>
2015-03-13 17:34:52 -0700

    created test_io.R

---
+++ tests/testthat/test_io.R
@@ -0,0 +1,26 @@
+#detach(package:vcfR, unload=TRUE)
+library(vcfR)
+context("io functions")
+
+data(vcfR_example)
+
+# Manage directories.
+original_dir <- getwd()
+test_dir <- tempdir()
+
+# Load data
+data(vcfR_example)
+
+test_that("vcf file io works",{
+  setwd(test_dir)
+
+  write.vcf(pinf_vcf, "test.vcf")
+  test <- read.vcf("test.vcf")
+  
+  expect_is(test, "vcfR")
+  expect_identical(names(test@fix)[1], "CHROM")
+  
+  setwd(original_dir)
+})
+
+
