9964131a4f88ba72ffd48d8c25c0c9aca778dc13
knausb <briank.lists@gmail.com>
2015-03-16 16:31:56 -0700

    created pair_sort.cpp, updated rank_variants.cpp

---
+++ src/pair_sort.cpp
@@ -0,0 +1,30 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+// Created for testing.
+
+typedef std::pair<int,int> mypair;
+bool comparator2 ( const mypair& l, const mypair& r)
+   { return l.first > r.first; }
+
+// [[Rcpp::export]]
+Rcpp::DataFrame pair_sort(){
+  std::vector < std::pair < int,int > > v_prime;
+  v_prime.push_back(std::make_pair(5, 0));
+  v_prime.push_back(std::make_pair(8, 1));
+  v_prime.push_back(std::make_pair(7, 2));
+  v_prime.push_back(std::make_pair(3, 3));
+  
+  Rcpp::NumericVector pair1;
+  Rcpp::NumericVector pair2;
+  
+  std::sort(v_prime.begin(), v_prime.end(), comparator2);
+  
+  for(int i=0; i<v_prime.size(); i++){
+    pair1.push_back(v_prime[i].first);
+    pair2.push_back(v_prime[i].second);
+  }
+  
+  return Rcpp::DataFrame::create(_["pair1"]=pair1, _["pair2"]=pair2);
+}
+
+
