6ca3b07b13639094e99d67b786abd58798fa27e2
knausb <briank.lists@gmail.com>
2015-08-24 07:20:18 -0700

    added conversion functions for genind and loci

--- R/vcf_conversion.R
+++ R/vcf_conversion.R
@@ -4,31 +4,40 @@
 #' @description
 #' Convert vcfR object to other formats
 #'  
-#' @param x A matrix of genotypes
+#' @param x an object of class Chrom or vcfR
+
 
 
 #' @rdname vcf_conversion
-#' @aliases gt_to_df
+#' @aliases vcfR2genind
+#' 
+#' @param sep character (to be used in a regular expression) to delimit the alleles of genotypes
 #' 
 #' @export
-gt_to_df <- function(x) {
-  fmat.col <- grep("FORMAT", names(x))
-  gt.pos <- grep("^GT$", unlist(strsplit(as.character(x[1, fmat.col]), ":")))
-  
-  gt2snpbin <- function(x) {
-    gt.a <- unlist(lapply(strsplit(as.character(x), ":"), function(x) {
-      x[gt.pos]
-    }))
-    gt.a <- unlist(lapply(strsplit(gt.a, "/"), function(x) {
-      paste(as.numeric(x[1]), as.numeric(x[2]), sep = "/")
-    }))
-    gt.a <- gsub("0", "2", gt.a)
-  }
-  gt.df <- data.frame(matrix(nrow = ncol(x) - fmat.col, ncol = nrow(x)))
-  for (i in c(fmat.col + 1):ncol(x)) {
-    gt.df[i - fmat.col, ] <- gt2snpbin(x[, i])
-  }
-  rownames(gt.df) <- colnames(x)[c(fmat.col + 1):ncol(x)]
-  gt.df
+vcfR2genind <- function(x, sep="[|/]") {
+  x <- extract.gt(x)
+  x <- adegenet::df2genind(t(x), sep=sep)
+  x
+}
+
+
+#' @rdname vcf_conversion
+#' @aliases vcfR2loci
+#' 
+#' @export
+vcfR2loci <- function(x)
+{
+#  if(class(x) == "Chrom")
+#  {
+#    x <- x@vcf
+#  }
+  x <- extract.gt(x)
+  # modified from pegas::as.loci.genind
+  x <- as.data.frame(t(x))
+  icol <- 1:ncol(x)
+  for (i in icol) x[, i] <- factor(x[, i] )
+  class(x) <- c("loci", "data.frame")
+  attr(x, "locicol") <- icol
+  x
 }
 
