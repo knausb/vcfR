ef2170eae025187136789797a1487cc79c089b7b
Brian J. Knaus <briank.lists@gmail.com>
2015-09-04 11:15:22 -0700

    added test_conversion.R

---
+++ tests/testthat/test_conversion.R
@@ -0,0 +1,59 @@
+
+
+#library(testthat)
+#detach(package:vcfR, unload=TRUE)
+library(vcfR)
+context("conversion functions")
+
+##### ##### ##### ##### #####
+# Example data.
+
+ex_file <- system.file("extdata", "pinf_sc1_100_sub.vcf.gz", package = "vcfR")
+seq_file <- system.file("extdata", "pinf_sc100.fasta", package = "vcfR")
+
+vcf <- read.vcf(ex_file, verbose=FALSE)
+dna <- ape::read.dna(seq_file, format = "fasta")
+
+gene <- dna[,159730:160890]
+
+pos <- as.numeric(vcf@fix[,'POS'])
+
+#vcf.gene <- vcf
+#vcf.gene@gt <- vcf.gene@gt[pos >= 159730 & pos <= 160890,]
+#vcf.gene@fix <- vcf.gene@fix[pos >= 159730 & pos <= 160890,]
+
+
+##### ##### ##### ##### #####
+
+test_that("vcfR2DANbin with consensus works",{
+  my_DNAbin <- vcfR2DNAbin( vcf, consensus = TRUE, gt.split = "/" )
+  
+  expect_true( inherits(my_DNAbin, "DNAbin") )
+  expect_equal( dim(my_DNAbin)[1], ncol(vcf@gt) - 1 )
+  expect_equal( dim(my_DNAbin)[2], nrow(vcf@gt) )
+})
+
+
+test_that("vcfR2DANbin with extract.haps works",{
+  my_DNAbin <- vcfR2DNAbin( vcf, consensus = FALSE, extract.haps = TRUE, gt.split = "/", verbose = FALSE )
+  
+  expect_true( inherits(my_DNAbin, "DNAbin") )
+  expect_equal( dim(my_DNAbin)[1], 2*(ncol(vcf@gt) - 1) )
+  expect_equal( dim(my_DNAbin)[2], nrow(vcf@gt) )
+})
+
+
+test_that("vcfR2DANbin with extract.haps works",{
+  my_DNAbin <- vcfR2DNAbin( vcf, consensus = FALSE, extract.haps = TRUE, 
+                            gt.split = "/", verbose = FALSE,
+                            ref.seq = gene, start.pos = 159730 )
+  
+  expect_true( inherits(my_DNAbin, "DNAbin") )
+  expect_equal( dim(my_DNAbin)[1], 2*(ncol(vcf@gt) - 1) )
+  expect_equal( dim(my_DNAbin)[2], dim(gene)[2] )
+})
+
+
+#debug(vcfR2DNAbin)
+
+
