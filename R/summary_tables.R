b51a8b0dafe87a5e11d9e44b48ea748947157595
Brian J. Knaus <briank.lists@gmail.com>
2015-10-28 16:46:34 -0700

    updated docs

--- R/summary_tables.R
+++ R/summary_tables.R
@@ -5,12 +5,12 @@
 #' @export
 #'
 #' @description
-#' Write summary tables from chromR objectst.
+#' Write summary tables from chromR objects.
 #' 
 #' @param file A filename for the output file
 #' @param x An object of class chromR
 #' @param mask logical vector indicating rows to use
-#' @param APPEND logical indicating whether to append to existing vcf file or write a new file
+#' @param APPEND logical indicating whether to append to existing file (omitting the header) or write a new file
 #'
 #' @details
 #' The function \strong{write.var.info} takes the variant information table from a chromR object and writes it as a comma delimited file. 
@@ -21,14 +21,14 @@
 #' @seealso
 #' \code{\link{write.vcf}}
 #'
-#' CRAN:
-#' \href{http://cran.r-project.org/web/packages/pegas/index.html}{pegas}::read.vcf,
-#' \href{http://cran.r-project.org/web/packages/PopGenome/index.html}{PopGenome}::readVCF,
-#' \href{http://cran.r-project.org/web/packages/data.table/index.html}{data.table}::fread
-#' 
-#' Bioconductor:
-#' \href{http://www.bioconductor.org/packages/release/bioc/html/VariantAnnotation.html}{VariantAnnotation}::readVcf
-#'
+# CRAN:
+# \href{http://cran.r-project.org/web/packages/pegas/index.html}{pegas}::read.vcf,
+# \href{http://cran.r-project.org/web/packages/PopGenome/index.html}{PopGenome}::readVCF,
+# \href{http://cran.r-project.org/web/packages/data.table/index.html}{data.table}::fread
+# 
+# Bioconductor:
+# \href{http://www.bioconductor.org/packages/release/bioc/html/VariantAnnotation.html}{VariantAnnotation}::readVcf
+#
 
 
 #' @rdname Summary.tables
