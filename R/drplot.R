f16ef6e1bdb43e4bb671eb27aff66e9c05f96b9b
knausb <briank.lists@gmail.com>
2015-11-30 18:09:48 -0800

    created drplot.R

---
+++ R/drplot.R
@@ -0,0 +1,17 @@
+
+#' @title dr plot elements
+#' @name  dr plot elements
+#'
+#' @description Plot chromR objects and their components
+#' @rdname chromo_elements
+#' 
+#' 
+#' @export
+dr.plot <- function( dmat, rlst,
+                     title = NULL, hline = NULL,
+                     dcol, rcol,
+                     ... ){
+  
+}
+
+
