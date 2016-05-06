554c22256c7770f616dccf585c12d1ad2f467541
Brian J. Knaus <briank.lists@gmail.com>
2016-05-05 17:06:34 -0700

    created ad_frequency.cpp

---
+++ src/ad_frequency.cpp
@@ -0,0 +1,100 @@
+#include <Rcpp.h>
+#include "vcfRCommon.h"
+
+// Helper for std::sort.
+struct greater
+{
+    template<class T>
+    bool operator()(T const &a, T const &b) const { return a > b; }
+};
+
+
+// Convert vectors of strings to floats.
+std::vector<float> str_vec_to_float_vec( std::vector<std::string> str_vec ){
+  // Initialize return vector.
+  std::vector<float> float_vec( str_vec.size(), 0 );
+  
+  int i;
+  for( i=0 ; i < str_vec.size() ; i++ ){
+  
+//    Rcpp::Rcout << "  " << str_vec[i] << "\n";
+    std::istringstream ss0(str_vec[i]);
+    if ( !( ss0 >> float_vec[i] ) ){
+      // error: didn't convert to a float
+      Rcpp::Rcerr << "Failed to convert to a float.\n";
+    }
+  }
+  return float_vec;
+}
+
+
+// [[Rcpp::export]]
+Rcpp::NumericMatrix AD_frequency(Rcpp::StringMatrix ad,
+                                 int allele = 1,
+                                 int sum_type = 0,
+                                 char delim = ',') {
+
+  // Initialize return data structure.
+  Rcpp::NumericMatrix adf( ad.nrow(), ad.ncol() );
+  adf.attr("dimnames") = ad.attr("dimnames");
+  
+  // NA matrix to return in case of unexpected results.
+  Rcpp::NumericMatrix nam( 1, 1 );
+  nam(0,0) = NA_REAL;
+    
+  if( allele < 1){
+    Rcpp::Rcerr << "Specified allele number is less than one.\n";
+    return nam;
+  }
+  
+  // R is one based, C++ zero based.
+  allele = allele - 1;
+
+  int i;
+  int j;
+
+  for(i=0; i<adf.nrow(); i++){   // Count rows (variants).
+    for(j=0; j<adf.ncol(); j++){ // Count columns (samples).
+
+      if( ad(i,j) != NA_STRING ){
+        std::vector < std::string > col_vec;
+        char my_split = ','; // Must be single quotes!
+        std::string my_string;
+        my_string = ad(i,j);
+        vcfRCommon::strsplit(my_string, col_vec, my_split);
+
+        // Recast vector of string to vector of floats.
+        std::vector < float > float_vec;( col_vec.size(), 0);
+        float_vec = str_vec_to_float_vec(col_vec);
+
+        // Sort the vector.
+        std::sort ( float_vec.begin(), float_vec.end(), greater() );
+        
+        // Calculate a sum.
+        float my_sum = 0;
+        if( sum_type == 0 ){
+          my_sum = float_vec[0] + float_vec[1];
+        } else if ( sum_type == 1 ){
+          for(std::vector<float>::iterator it = float_vec.begin(); it != float_vec.end(); ++it)
+            my_sum += *it;
+        } else {
+          Rcpp::Rcerr << "Undefined sum type.\n";
+          return nam;
+        }
+        
+        // Calculate the frequency.
+        if( allele < float_vec.size() ){
+          adf(i,j) = float_vec[ allele ]/my_sum;
+        } else {
+          adf(i,j) = NA_REAL;
+        }
+        
+      } else {
+        adf(i,j) = NA_REAL;
+      }
+    }
+  }
+
+  return adf;
+}
+
