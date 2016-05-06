e7cc6e88cd3bb1278a5c9523a420a9b57fa16c63
knausb <briank.lists@gmail.com>
2016-05-05 18:52:34 -0700

    worked on docs for AD_frequency

--- src/ad_frequency.cpp
+++ src/ad_frequency.cpp
@@ -28,11 +28,33 @@ std::vector<float> str_vec_to_float_vec( std::vector<std::string> str_vec ){
 }
 
 
-// [[Rcpp::export]]
+//' @title AD_frequency
+//' @name AD_frequency
+//' @rdname AD_frequency
+//'
+//' @description
+//' Create allele frequencies from matrices of allelic depths (AD)
+//'
+//' @param ad a matrix of allele depths (e.g., "7,2")
+//' @param allele which (1-based) allele to report frequency for
+//' @param sum_type type of sum to calculate, see details
+//' @param delim character that delimits values
+//'
+//' @details
+//' To do.
+//'
+//' There are two methods to calculate a sum for the denominator of the frequency.
+//' When sum_type = 0 the alleles are sorted decendingly and the first two allele counts are used for the sum.
+//' This may be useful when a state of diploidy may be known to be appropriate and other alleles may be interpreted as erroneous.
+//' When sum_type = 1 a sum is taken over all the observed alleles for a variant.
+//'
+//' @return a numeric matrix of frequencies
+//' [[Rcpp::export]]
 Rcpp::NumericMatrix AD_frequency(Rcpp::StringMatrix ad,
                                  int allele = 1,
                                  int sum_type = 0,
-                                 char delim = ',') {
+                                 char delim = ','
+                                 ) {
 
   // Initialize return data structure.
   Rcpp::NumericMatrix adf( ad.nrow(), ad.ncol() );
