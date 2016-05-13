4699a7437c6a489a8113e7761e770171fe2c61c8
Brian J. Knaus <briank.lists@gmail.com>
2016-05-13 11:53:53 -0700

    added vcfR_test data set

---
+++ R/vcfR_test.R
@@ -0,0 +1,31 @@
+#' Test data for vcfR.
+#' 
+#' A test file containing a diversity of examples inended to tst functionality.
+#' 
+#' \itemize{
+#'   \item vcfR_test vcfR object
+#' }
+#' 
+#' 
+#' This data set began as the example (section 1.1) from The Variant Call Format Specification \href{http://samtools.github.io/hts-specs/}{VCFv4.3} .
+#' This data consisted of 3 samples and 5 variants.
+#' As I encounter examples that challenge the code in vcfR they can be added to this data set.
+#' 
+#' 
+#' 
+#' @examples
+#' data(vcfR_test)
+#' 
+#' 
+#' \dontrun{
+#'   # When I add data it can be saved with this command.
+#'   save(vcfR_test, file="data/vcfR_test.RData") 
+#'   }
+#' 
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A vcfR object
+#' @name vcfR_test
+#' @aliases vcf_test
+NULL
