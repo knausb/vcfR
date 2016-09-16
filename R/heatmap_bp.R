311ae024a42c4d25f23d9f36c24124152da0c282
Brian J. Knaus <briank.lists@gmail.com>
2016-09-16 15:46:29 -0700

    implemented cbarplot, rbarplot and legend in heatmap.bp

--- R/heatmap_bp.R
+++ R/heatmap_bp.R
@@ -37,6 +37,18 @@
 #' heatmap.bp(x, col.ramp = colorRampPalette(c("red", "yellow", "#008000"))(100))
 # library(viridis)
 #' heatmap.bp(x)
+#' 
+#' \dontrun{
+#' heatmap.bp(x, cbarplot = FALSE, rbarplot = FALSE, legend = FALSE)
+#' heatmap.bp(x, cbarplot = FALSE, rbarplot = TRUE, legend = FALSE)
+#' heatmap.bp(x, cbarplot = FALSE, rbarplot = FALSE, legend = TRUE)
+#' heatmap.bp(x, cbarplot = FALSE, rbarplot = TRUE, legend = TRUE)
+#' 
+#' heatmap.bp(x, cbarplot = TRUE, rbarplot = FALSE, legend = FALSE)
+#' heatmap.bp(x, cbarplot = TRUE, rbarplot = TRUE, legend = FALSE)
+#' heatmap.bp(x, cbarplot = TRUE, rbarplot = FALSE, legend = TRUE)
+#' heatmap.bp(x, cbarplot = TRUE, rbarplot = TRUE, legend = TRUE)
+#' }
 #'
 #'  
 # data(vcfR_example)
@@ -62,13 +74,15 @@ heatmap.bp <- function(x, cbarplot = TRUE, rbarplot = TRUE,
     "none"
   else match.arg(scale)
   #
+  
+  # Determine the geometry of the plot.
   nrows <- 1
   ncols <- 1
   if(cbarplot == TRUE){ nrows <- nrows + 1 }
   if(rbarplot == TRUE){ ncols <- ncols + 1 }
-  if(rbarplot == TRUE){ ncols <- ncols + 1 }
-  #
-  #
+  if(legend == TRUE){ ncols <- ncols + 1 }
+  
+  # Scale the data as appropriate.
   if (scale == "row") {
     x <- sweep(x, 1L, rowMeans(x, na.rm = na.rm), check.margin = FALSE)
     sx <- apply(x, 1L, stats::sd, na.rm = na.rm)
@@ -79,25 +93,49 @@ heatmap.bp <- function(x, cbarplot = TRUE, rbarplot = TRUE,
     sx <- apply(x, 2L, stats::sd, na.rm = na.rm)
     x <- sweep(x, 2L, sx, "/", check.margin = FALSE)
   }
-  #
+  
+  # Handle column names.
   if(is.null(colnames(x))){colnames(x) <- 1:ncol(x)}
   if(is.null(rownames(x))){rownames(x) <- 1:nrow(x)}
-  #
-  if(ncols == 1 & nrows == 1){
-    cat("1 and 1\n")
-    graphics::image(t(x),
-          col = col.ramp,          
-          axes=FALSE, frame.plot=TRUE)
+
+  # Set plot geometry.
+  if( cbarplot == FALSE & rbarplot == FALSE & legend == FALSE ){
+    # One panel.
   }
-  if(nrows == 1 & ncols == 2){}
-  if(nrows == 1 & ncols == 3){}
-  if(nrows == 2 & ncols == 1){}
-  if(nrows == 2 & ncols == 2){}
-  if(nrows == 2 & ncols == 3){
-    graphics::layout(matrix(1:6,nrow=nrows, ncol=ncols, byrow = TRUE),
+  if( cbarplot == FALSE & rbarplot == TRUE & legend == FALSE ){
+     graphics::layout(matrix(1:2, nrow=nrows, ncol=ncols, byrow = TRUE),
+           widths=c(4, 0.6))
+  }
+  if( cbarplot == FALSE & rbarplot == FALSE & legend == TRUE ){
+     graphics::layout(matrix(1:2, nrow=nrows, ncol=ncols, byrow = TRUE),
+           widths=c(4, 0.2))
+  }
+  if( cbarplot == FALSE & rbarplot == TRUE & legend == TRUE ){
+     graphics::layout(matrix(1:3, nrow=nrows, ncol=ncols, byrow = TRUE),
+           widths=c(4, 0.6, 0.2))
+  }
+  if( cbarplot == TRUE & rbarplot == FALSE & legend == FALSE ){
+    graphics::layout(matrix(1:2, nrow=nrows, ncol=ncols, byrow = TRUE),
+           heights=c(1, 4))    
+  }
+  if( cbarplot == TRUE & rbarplot == TRUE & legend == FALSE ){
+    graphics::layout(matrix(1:4, nrow=nrows, ncol=ncols, byrow = TRUE),
+           widths=c(4, 0.6), heights=c(1, 4))
+  }
+  if( cbarplot == TRUE & rbarplot == FALSE & legend == TRUE ){
+    graphics::layout(matrix(1:4, nrow=nrows, ncol=ncols, byrow = TRUE),
+           widths=c(4, 0.2), heights=c(1, 4))
+  }
+  if( cbarplot == TRUE & rbarplot == TRUE & legend == TRUE ){
+    graphics::layout(matrix(1:6, nrow=nrows, ncol=ncols, byrow = TRUE),
            widths=c(4, 0.6, 0.2), heights=c(1, 4))
-    graphics::par(mar=c(0,0,0,0))
-    graphics::par(oma=c(1,1,1,1))
+  }
+  
+  # Global parameters.
+  graphics::par(mar=c(0,0,0,0))
+  graphics::par(oma=c(1,1,1,1))
+  
+  if( cbarplot == TRUE ){
     graphics::barplot(colSums(x, na.rm=na.rm),
             space=0, border=NA, axes=FALSE,
             names.arg="",
@@ -107,13 +145,20 @@ heatmap.bp <- function(x, cbarplot = TRUE, rbarplot = TRUE,
     } else if (clabels == TRUE & scale != 'none'){
       graphics::text(c(1:ncol(x))-0.5, min(colSums(x, na.rm=na.rm), na.rm=na.rm), colnames(x), adj=c(0.0,0.5), srt=90)
     }
-    #
-    plot(1, 1, type="n", axes=FALSE, xlab="", ylab="")
-    plot(1, 1, type="n", axes=FALSE, xlab="", ylab="")
-    graphics::image(t(x),
-          col = col.ramp,
-          axes=FALSE, frame.plot=TRUE)
-    #
+    if( rbarplot == TRUE ){
+      plot(1, 1, type="n", axes=FALSE, xlab="", ylab="")
+    }
+    if( legend == TRUE ){
+      plot(1, 1, type="n", axes=FALSE, xlab="", ylab="")
+    }
+  }
+  
+  # Plot image matrix.
+  graphics::image(t(x), col = col.ramp,
+                  axes=FALSE, frame.plot=TRUE)
+  
+  # Row barplot.
+  if( rbarplot == TRUE ){
     graphics::barplot(rowSums(x, na.rm=na.rm),
             space=0, border=NA,
             horiz=TRUE, axes=FALSE, names.arg="",
@@ -121,19 +166,24 @@ heatmap.bp <- function(x, cbarplot = TRUE, rbarplot = TRUE,
             yaxs="i")
     if(rlabels == TRUE & scale == 'none'){
       graphics::text(0, c(1:nrow(x))-0.5, rownames(x), adj=c(0.0, 0.5), srt=0)
-      } else if(rlabels == TRUE & scale != 'none'){
+    } else if(rlabels == TRUE & scale != 'none'){
       graphics::text(min(rowSums(x, na.rm=na.rm), na.rm=na.rm), c(1:nrow(x))-0.5, rownames(x), adj=c(0.0,0.5), srt=0)
     }
-    #
+  }
+  
+  # Legend.
+  if( legend == TRUE ){
     mp <- graphics::barplot(rep(1, times=length(col.ramp)), space=0, border=NA, horiz = TRUE,
                             col = col.ramp, axes=FALSE)
 #    graphics::text(0.5, 5, "Low", col="#FFFFFF")
 #    graphics::text(0.5, 95, "High", col="#FFFFFF")
     if ( mp[nrow(mp),1] - mp[1,1] >= 1 ){
-      graphics::text(0.5, mp[1,1], "Low", col="#FFFFFF")
-      graphics::text(0.5, mp[nrow(mp),1], "High", col="#FFFFFF")
+      graphics::text(0.5, mp[1,1], "Low", col="#FFFFFF", adj=c(0.5,0))
+      graphics::text(0.5, mp[nrow(mp),1], "High", col="#FFFFFF", adj=c(0.5,1))
     }
   }
+  
+  # Reset graphics device.
   graphics::par(mfrow=c(1,1))
   graphics::par(mar=c(5,4,4,2))
   #
