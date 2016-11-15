ab4e15cc49558b295d56352f233e42d670db8046
Brian J. Knaus <briank.lists@gmail.com>
2016-11-15 09:47:05 -0800

    .onAttach changed packageDescription to utils::packageVersion

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onAttach <- function(libname, pkgname){
-  pkg.version <- packageDescription("vcfR", fields = "Version")
-
+#  pkg.version <- packageDescription("vcfR", fields = "Version")
+  pkg.version <- utils::packageVersion("vcfR")
+  
   startup.txt <- paste("\n",
                        "   >>> This is vcfR ", pkg.version, " <<<\n",
                        "   > To cite: citation('vcfR')\n",
