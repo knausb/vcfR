2f9cf615b09d9c6a7d6fc5169cb12be02401bead
Brian J. Knaus <briank.lists@gmail.com>
2017-09-06 13:08:35 -0700

    minimal package builds

--- tools/src/vcf_io.cpp
+++ tools/src/vcf_io.cpp
@@ -11,7 +11,7 @@ const int nreport = 1000;
 #define LENGTH 0x1000 // hexadecimel for 4096.
 
 
-/*  Helper functions */
+/* Helper functions */
 
 /* 
 Called by vcf_stats_gz.
@@ -668,51 +668,6 @@ int verbose = 0;
 }
 
 
-
-/* Write data to fasta file */
-
-// [[Rcpp::export]]
-void write_fasta( Rcpp::CharacterVector seq,
-                  std::string seqname, 
-                  std::string filename, 
-                  int rowlength=80,
-                  int verbose=1) {
-//  rowlength=rowlength-1;
-  FILE * pFile;
-//  pFile=fopen(filename.c_str(),"wt");
-  pFile=fopen(filename.c_str(),"at");
-  int i = 0;
-//  unsigned int i = 0;
-  
-  if(verbose == 1){
-    Rcpp::Rcout << "Processing sample: " << seqname << "\n";
-  }
-
-  putc ('>' , pFile);
-  for(i=0; (unsigned)i<seqname.size(); i++){
-    putc (seqname[i] , pFile);
-  }
-  putc ('\n' , pFile);
-
-  putc (Rcpp::as< char >(seq[0]) , pFile);
-  for(i=1; i<seq.size(); i++){
-    Rcpp::checkUserInterrupt();
-//    putc (seq[i][0] , pFile);
-    if( i % rowlength == 0){
-      putc('\n', pFile);
-    }
-    putc (Rcpp::as< char >(seq[i]) , pFile);
-    if(i % nreport == 0 && verbose == 1){
-      Rcpp::Rcout << "\rNucleotide " << i << " processed";
-    }
-  }
-  putc('\n', pFile);
-  fclose (pFile);
-  if(verbose == 1){
-    Rcpp::Rcout << "\rNucleotide " << i << " processed\n";
-  }
-//  return 0;
-}
 
 
 
