5f4b2d53a0f32362de177e979a9f14cd8a7538a5
Brian Knaus <briank.lists@gmail.com>
2021-08-23 13:30:41 -0700

    created vcfR2hapmap

---
+++ R/vcfR2hapmap.R
@@ -0,0 +1,34 @@
+#' 
+#' @title Convert a vcfR object to hapmap
+#' @description The function converts a vcfR object to hapmap
+#'
+#' @param vcf a vcfR object.
+# ' @param out_file name of output file.
+# ' @param method should 'N' or 'H' format data be generated?
+#' 
+#' 
+#'
+#' @details
+#' This function converts a vcfR object to a text file which can be used as input for MigrateN.
+#' 
+#' @return a text file that can be used as an input for MigrateN software (SNP format).
+#' 
+#' 
+#' 
+#' 
+#' @author Brian J. Knaus
+#' 
+#' 
+#' @seealso \href{http://popgen.sc.fsu.edu/Migrate/Migrate-n.html}{Migrate-N} website.
+#'
+# ' @examples
+#' 
+#' 
+#' 
+#' @export
+vcfR2hapmap <- function(vcf) {
+  print("vcfR2hapmap works!")  
+  
+}
+  
+
